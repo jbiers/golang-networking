1caceae26de31dee6bda96424b31b14108dd4758
julia <juliabiersuriano@gmail.com>
2023-04-10 21:01:13 -0400

    can send data from file

---
+++ week02_tcp/exercises/client.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		log.Fatal("Usage: go run client.go filename01 filename02 ...")
+	}
+
+	currPath, _ := os.Getwd()
+
+	file, err := os.Open(currPath + "/" + args[0])
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal("Could not open file:", err.Error())
+	}
+
+	buffer := make([]byte, 1024)
+	n, err := file.Read(buffer)
+	if err != nil {
+		log.Fatal("Could not read file:", err.Error())
+	}
+
+	fmt.Println(n)
+
+	addr := "127.0.0.1:8080"
+	conn, err := net.Dial("tcp", addr)
+
+	if err != nil {
+		log.Fatal("Error connecting:", err.Error())
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Connecting to TCP server in", addr)
+
+	_, err = conn.Write([]byte(buffer))
+
+	if err != nil {
+		log.Fatal("Error sending message:", err.Error())
+	}
+}
+
+// at this point, it's tricky to understand how client and server buffer sizes interact
---
+++ week02_tcp/exercises/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	addr := "127.0.0.1:8080"
+
+	listener, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatal("Error creating listener:", err.Error())
+	}
+
+	defer listener.Close()
+
+	fmt.Println("TCP Server listening on", addr)
+
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Println("Error accepting connection:", err.Error())
+	}
+
+	fmt.Println("New connection from", conn.RemoteAddr())
+
+	err = handleConnection(conn)
+	if err != nil {
+		log.Println("Error accepting connection:", err.Error())
+	}
+}
+
+func handleConnection(conn net.Conn) error {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Received data:", string(buffer[:n]))
+
+	return nil
+}
